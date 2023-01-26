52eb551eba3afc4afef7e85f49665cd2b6c1ce24
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-26 16:46:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+const clock = document.querySelector(".clock-face");
+const hoursHand = clock.querySelector(".hour-hand");
+const minutesHand = clock.querySelector(".min-hand");
+const secondsHand = clock.querySelector(".second-hand");
+
+
+function updateTime() {
+  const currentDate = new Date();
+
+   // referenced ukdgit repository (although clock was working 100% the test cases aren't passing)
+  const seconds = currentDate.getSeconds();
+  const minutes = currentDate.getMinutes();
+  const hours = currentDate.getHours();
+
+  const hoursDegrees = 30 * hours + minutes / 2;;
+  const minutesDegrees = 6 * minutes;
+  const secondsDegrees = 6* seconds;
+
+  secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
